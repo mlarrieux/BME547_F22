a2c12800a8e2360268d41129192b9dccd7f703b7
“Mimi <micaelle.larrieux@duke.edu>
2022-09-14 13:16:35 -0400

    in class work from 9/14. worked on lists, tuples, searching, enumerate etc

--- README.md
+++ README.md
@@ -1,9 +1,36 @@
-# BME547_F22
+# Patient Health Database
 
-This repo will be used throughout the Fall of 2022 for the course BME547.
+Author: Mimi Larrieux
 
-###### I will now practice some markdown language:
+## Purpose
+The code in this repository will run a patient health database.
+The code is written in python.
 
-_This is Italic_
-__This is bold__
+## Specifications
+The database will contain:
+* Patient Name
+    + First Name
+    + Last Name
+* Age
+* Patient ID
+* Test Results
+    * Test name
+    * Test Result
+    1. HDL
+    1. LDL
 
+# Future Improvements
+* Add blood type
+
+### Task:
+Write a function that takes the `db` variable as
+a parameter and print ot each patient information
+on a line, nicely formatted.
+
+Write a function that takes the database and a patient
+id, have the function look through the database and 
+return the patient who matches the id.
+
+Write a function taht takes the database, patient id,
+test name, and test value as parameters, and add those
+test results to the appropriate patient.
---
+++ database.py
@@ -0,0 +1,41 @@
+def create_patient_entry(patient_name, patient_id, patient_age):
+    new_patient = [patient_name, patient_id, patient_age, []]
+    return new_patient
+
+def main():
+    db = []
+    db.append(create_patient_entry("Ann Able", 1, 30))
+    db.append(create_patient_entry("Bob Boyles", 2, 34))
+    db.append(create_patient_entry("Chris Chou", 3, 25))
+    add_test_to_patient(db, 3, "HDL", 100)
+    print(find_patient(db, 3))
+    print(db)
+
+    room_list = ["room 1", "room 2", "room 3", "room 4"]
+
+    for i, patient in enumerate(db):
+        print(i)
+        print("name = {}, Room = {}".format(patient[0], room_list[i]))
+
+    for patient, room in zip(db, room_list):
+        print("Name = {}, Room = {}".format(patient[0], room))
+
+    
+def print_database(db):
+    print("This is an output of all patients and information:\n\n")
+    for patient in db:
+        name = patient[0].split(" ")
+        print("First Name: {}\t Last Name: {}\t Patient ID: {}\t Patient Age: {}".format(name[0], name[1], patient[1], patient[2]))
+
+def find_patient(db, id_no):
+    for patient in db:
+        if patient[1] == id_no:
+            return patient
+    return False
+
+def add_test_to_patient(db, id_no, test_name, test_value):
+    patient = find_patient(db, id_no)
+    patient[3].append((test_name, test_value))
+
+if __name__ == "__main__":
+    main()
