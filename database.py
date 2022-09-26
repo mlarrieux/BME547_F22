6c61a33993055d860ccf7b550d5230bf568d2d6b
“Mimi <micaelle.larrieux@duke.edu>
2022-09-26 12:42:23 -0400

    convert patient recrod to dictionary that will be stored in database list

--- database.py
+++ database.py
@@ -1,45 +1,53 @@
-def create_patient_entry(patient_name, patient_id, patient_age):
-    new_patient = [patient_name, patient_id, patient_age, []]
+def create_patient_entry(patient_first_name, patient_last_name, patient_id, 
+                         patient_age):
+    new_patient = {'first name': patient_first_name,
+                   'last name': patient_last_name,
+                   "ID": patient_id, 'age': patient_age, 'test_results': ''}
     return new_patient
 
 
-def main():
-    db = []
-    db.append(create_patient_entry("Ann Able", 1, 30))
-    db.append(create_patient_entry("Bob Boyles", 2, 34))
-    db.append(create_patient_entry("Chris Chou", 3, 25))
-    add_test_to_patient(db, 3, "HDL", 100)
-    print(find_patient(db, 3))
-    print(db)
+def get_full_name(patient):
+    return patient['first name'] + ' ' + patient['last name']
 
-    room_list = ["room 1", "room 2", "room 3", "room 4"]
 
-    for i, patient in enumerate(db):
-        print(i)
-        print("name = {}, Room = {}".format(patient[0], room_list[i]))
-
-    for patient, room in zip(db, room_list):
-        print("Name = {}, Room = {}".format(patient[0], room))
+def adult_or_minor(patient):
+    if patient['age'] >= 18:
+        return 'adult'
+    else:
+        return 'minor'
 
 
 def print_database(db):
     print("This is an output of all patients and information:\n\n")
     for patient in db:
-        name = patient[0].split(" ")
         print("First Name: {}\t Last Name: {}\t Patient ID: {}\t Patient\
-             Age: {}".format(name[0], name[1], patient[1], patient[2]))
+             Age: {}".format(patient['first name'], patient['last name'],
+              patient["ID"], patient['age']))
 
 
 def find_patient(db, id_no):
     for patient in db:
-        if patient[1] == id_no:
+        if patient["ID"] == id_no:
             return patient
     return False
 
 
 def add_test_to_patient(db, id_no, test_name, test_value):
     patient = find_patient(db, id_no)
-    patient[3].append((test_name, test_value))
+    patient['test_results'] = (test_name, test_value)
+
+
+def main():
+    db = []
+    db.append(create_patient_entry("Ann", "Able", 1, 30))
+    db.append(create_patient_entry("Bob", "Boyles", 2, 34))
+    db.append(create_patient_entry("Chris", "Chou", 3, 25))
+    add_test_to_patient(db, 3, "HDL", 100)
+    print(find_patient(db, 3))
+    print(db)
+
+    print("Patient {} is a {}".format(get_full_name(db[2]),
+                                      adult_or_minor(db[2])))
 
 
 if __name__ == "__main__":
