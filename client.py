0247763d01d86d8b0bba33e3a4de461bbaf990f6
“Mimi <micaelle.larrieux@duke.edu>
2022-11-06 21:00:11 -0500

    adding stuff from class

--- client.py
+++ client.py
@@ -2,15 +2,61 @@
 
 import requests
 
-in_data = {'date': "12/12/1969", 'units': "years"}
+in_data = {'patient_id': 453, 'attending_username': 'mimi.m', 'patient_age': 5}
 
-r = requests.post("http://127.0.0.1:5000/age", json=in_data)
+# konrad type here
+in_data = {'attending_username': "konrad.m",
+           'attending_email': 'kon@gmail.com',
+           'attending_phone': "304-444-9090"}
+
+r = requests.post("http://vcm-29248.vm.duke.edu:5000/api/new_attending",
+                  json=in_data)
 print(r.status_code)
 print(r.text)
 
+in_data = {'patient_id': "1",
+           'heart_rate': '50'}
 
-# out_data = {'a': 5, 'b': 9}
+r = requests.post("http://vcm-29248.vm.duke.edu:5000/api/heart_rate",
+                  json=in_data)
+print(r.status_code)
+print(r.text)
 
-# r = requests.post("http://127.0.0.1:5000/add_numbers", json=out_data)
-# print(r.status_code)
-# print(r.text)
+r = requests.get("http://vcm-29248.vm.duke.edu:5000/api/status/1")
+print(r.status_code)
+print(r.text)
+# ******************************************************************************
+
+# mimi type here
+
+r = requests.get("http://vcm-29248.vm.duke.edu:5000/api/patients/ferd.j")
+print(r.status_code)
+print(r.text)
+
+in_data = {"patient_id": '24',
+           "attending_username": 'lemon.b',
+           "patient_age": '22'}
+
+r = requests.post("http://vcm-29248.vm.duke.edu:5000/api/new_patient",
+                  json=in_data)
+print(r.status_code)
+print(r.text)
+
+r = requests.get("http://vcm-29248.vm.duke.edu:5000/api/heart_rate/1")
+print(r.status_code)
+print(r.text)
+
+r = requests.get("http://vcm-29248.vm.duke.edu:5000/api/heart_rate/average/2")
+print(r.status_code)
+print(r.text)
+
+in_data = {'patient_id': 4, 'heart_rate_average_since': '2018-09-18 13:55:26'}
+
+r = requests.post("http://vcm-29248.vm.duke.edu:5000/api/heart_rate/'\
+                  'interval_average", json=in_data)
+print(r.status_code)
+print(r.text)
+
+r = requests.get("http://vcm-29248.vm.duke.edu:5000/api/patients/mimi.m")
+print(r.status_code)
+print(r.text)
---
+++ db_client.py
@@ -0,0 +1,11 @@
+import requests
+
+out_data = {"name": "Charlie", "id": 3, "blood_type": "AB-"}
+r = requests.post("http://127.0.0.1:5000/new_patient", json=out_data)
+print(r.status_code)
+print(r.text)
+
+test_data = {'id': 2, 'test_name': 'HDL', 'test_result': 100}
+r = requests.post("http://127.0.0.1:5000/new_patient", json=test_data)
+print(r.status_code)
+print(r.text)
